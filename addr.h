8fb051db9759d6c22ed0c85e49399111d4ba3bef
cybergoatpsyops <leo@leobastidas.com>
2019-06-23 14:45:16 -0500

    learning vars

---
+++ addr.h
@@ -0,0 +1,3 @@
+printf("\n6104 E. Oak\n");
+printf("St. Paul, MN\n");
+printf("	54245\n");
---
+++ learningVars.c
@@ -0,0 +1,31 @@
+// Example program about local and global variables
+// File learningVars.c
+
+/* The program is a simple demonstration of the difference between
+ * global variables and local variables. */
+
+#include <stdio.h>
+int g1 = 10;
+
+int main()
+{
+	float l1;
+	l1 = 9.0;
+
+	printf("%d %.2f\n", g1, l1);	// prints the 1st global and first
+					// local variable
+	prAgain(); //calls our first function
+	return 0;
+}
+
+float g2 = 19.0;
+
+int prAgain()
+{
+	int l2;
+	l2 = 5;
+
+	// Can't print 11--it is local to main
+	printf("%d %.2f %d\n", l2, g2, g1);
+	return 0;
+}
